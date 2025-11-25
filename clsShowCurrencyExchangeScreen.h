87c9760bc41825ffbab73e8f4746f7434e683341
Mohamed Ragheb <mohamedraghebomer@gmail.com>
2025-11-25 07:18:18 +0200

    finish extintion/add-currency-exchange-menue (add Currency Main Screen), and add Currencies.txt

---
+++ clsShowCurrencyExchangeScreen.h
@@ -0,0 +1,109 @@
+#pragma once
+
+#include "clsScreen.h"
+
+class clsShowCurrencyExchangeScreen : public clsScreen
+{
+private:
+
+    static short _ReadCurrencyExchangeMenueChoose()
+    {
+        return clsInputValidate::ReadShortNumberBetween(1, 5, "Choose what do you want to do from [1 To 5]: ", "Invalid Choose, please try again.\n\n");
+    }
+
+    enum _enCurrencyExchangeMenueOptions
+    {
+        eListCurrencies = 1, eFindCurrency = 2, eUpdateRate = 3,
+        eCurrencyCalculator = 4, eMainMenue = 5
+    };
+
+    static void _GoToCurrencyExchangeMenueScreen()
+    {
+        cout << "Press any key to go to Currency Exchange Menue Screen...";
+        system("pause>0");
+        system("cls");
+        ShowCurrencyExchangeMenueScreen();
+    }
+
+    static void _ShowListCurrenciesScreen()
+    {
+        cout << "Show List Currencies will be here" << endl;
+    }
+
+    static void _ShowFindCurrencyScreen()
+    {
+        cout << "Show Find Currency Screen will be here" << endl;
+    }
+
+    static void _ShowUpdateRateScreen()
+    {
+        cout << "Show update rate Screen will be here" << endl;
+    }
+
+    static void _ShowCurrencyCalculatorScreen()
+    {
+        cout << "Show Currency Calculator Screen will be here" << endl;
+    }
+
+    static void _PerformCurrencyExchangeMenueChoose(_enCurrencyExchangeMenueOptions Choose)
+    {
+        switch (Choose)
+        {
+        case eListCurrencies:
+        {
+            system("cls");
+            _ShowListCurrenciesScreen();
+            _GoToCurrencyExchangeMenueScreen();
+            break;
+        }
+        case eFindCurrency:
+        {
+            system("cls");
+            _ShowFindCurrencyScreen();
+            _GoToCurrencyExchangeMenueScreen();
+            break;
+        }
+        case eUpdateRate:
+        {
+            system("cls");
+            _ShowUpdateRateScreen();
+            _GoToCurrencyExchangeMenueScreen();
+            break;
+        }
+        case eCurrencyCalculator:
+        {
+            system("cls");
+            _ShowCurrencyCalculatorScreen();
+            _GoToCurrencyExchangeMenueScreen();
+            break;
+        }
+        case eMainMenue:
+        {
+            // do nothing, it will work as expected...
+            break;
+        }
+        default:
+        {
+            system("cls");
+            cout << "We are sorry, an expected error oqoured." << endl;
+            // after that it will go to main menue screen
+        }
+        
+        }
+    }
+
+public:
+
+	static void ShowCurrencyExchangeMenueScreen()
+	{
+        clsScreen::_DrawScreenHeader("Currency Exchange Screen");
+        cout << "\t[1] List Currencies.\n";
+        cout << "\t[2] Find Currency.\n";
+        cout << "\t[3] Update Rate.\n";
+        cout << "\t[4] Currency Calculator.\n";
+        cout << "\t[5] Main Menue.\n";
+        cout << "===========================================\n";
+        _PerformCurrencyExchangeMenueChoose((_enCurrencyExchangeMenueOptions)_ReadCurrencyExchangeMenueChoose());
+	}
+};
+
